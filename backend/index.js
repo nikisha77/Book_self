66e78c9036a83615caaf208f6b8dad15f9513d7d
Nikisha <knikisha77@gmail.com>
2024-12-02 07:47:41 +0530

    Initial folders for starter pack of project

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,12 @@ import express from "express";
 const PORT = 3000
 const app = express()
 
+app.get('/',(req , res)=>{
+    console.log(req)
+
+    return res.status(234).send("this page has been update")
+})
+
 app.listen(PORT , ()=>{
     console.log(`Server is running on port ${PORT}`)
 })
